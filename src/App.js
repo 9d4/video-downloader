137c9b2498f6e98181458ca23a07550026d0439a
Dimas Ananda <traperwaze@gmail.com>
2020-08-17 22:16:26 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './App.css'
+
+class App extends React.Component {
+    render(){
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
