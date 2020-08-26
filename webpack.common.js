30591b2e5a8c8b8e7eb1e08b18bd6476cdafbd6f
Dimas Ananda <traperwaze@gmail.com>
2020-08-26 07:56:30 +0700

    setup development env

--- webpack.common.js
+++ webpack.common.js
@@ -13,7 +13,14 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                    }
+                ]
             },
             {
                 test: /\.css$/,
---
+++ webpack.dev.js
@@ -0,0 +1,8 @@
+const webpack = require("webpack");
+
+const common = require('./webpack.common')
+const merger = require('webpack-merge')
+
+module.exports = merger.merge(common, {
+    mode: 'development'
+})
