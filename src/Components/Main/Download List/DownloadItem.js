e304553a662dd0a22b6d3c2acaef13cdc9b3b950
Dimas Ananda <traperwaze@gmail.com>
2020-08-28 22:30:10 +0700

    nothing

--- src/Components/Main/Download List/DownloadItem.js
+++ src/Components/Main/Download List/DownloadItem.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export default class DownloadItem extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
     render() {
         return (
-            <div></div>
+            <div className='col my-2 d-flex justify-content-center'>
+                <iframe
+                    style={{ width: '230px', height: '60px', border: 0, overflow: 'hidden' }}
+                    scrolling="no"
+                    src={`https://loader.to/api/button/?url=${this.props.url}&f=${this.props.format.toLowerCase()}`}
+                ></iframe>
+            </div>
         )
     }
 }
