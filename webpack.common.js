c21bfb47982ef91b76ac8eac73349374b712cfdb
Dimas Ananda <traperwaze@gmail.com>
2020-08-28 07:12:21 +0700

    move publish dir to public/

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'public/dist')
     },
     module: {
         rules: [
